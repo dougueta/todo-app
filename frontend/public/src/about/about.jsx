d9c81e555255ada3b70a7bd6062e806510623418
dougueta <douglas@guiabolso.com.br>
2019-02-04 23:24:29 -0200

    :construction: making some exercices

--- frontend/public/src/about/about.jsx
+++ frontend/public/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
-    </div>
+        <PageHeader name='SObre' small='nós' />
+
+        <h2>Nossa História</h2>
+        <p>Texto falando da nossa história</p>
+
+        <h2>Missão e visão</h2>
+        <p>Texto falando da nossa Missão e visão</p>
+
+        <h2>Imprensa</h2>
+        <p>Texto falando da imprensa</p>
+    </div> 
 )

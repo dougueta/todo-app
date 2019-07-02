3a4722bbe059397f2eb8d8a14ac2a5aa43bc1d8a
dougueta <douglas@guiabolso.com.br>
2019-07-02 08:41:24 -0300

    some changes

--- frontend/public/src/todo/todoActions.js
+++ frontend/public/src/todo/todoActions.js
@@ -1,4 +1,12 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:303/api/todos'
+
 export const changeDescription = (event) => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const search = () => {
+    const request = axios.get(`${URL}?SORT=-createdAt`)
+}
